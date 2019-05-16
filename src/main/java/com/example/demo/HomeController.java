3cbbd36710804ca753098cf9ddc7e77fe0e31620
GBTC406009UR <GBTC406009UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-05-16 15:29:30 -0400

    message

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -31,6 +32,25 @@ public class HomeController {
             return "todoForm";
         }
         todoRepository.save(todo);
+        return "redirect:/";
+    }
+
+    @RequestMapping("/detail/{id}")
+    public String showTODO(@PathVariable("id") long id, Model model)
+    {
+        model.addAttribute("todo", todoRepository.findById(id).get());
+        return "show";
+    }
+
+    @RequestMapping("/update/{id}")
+    public String updateTODO(@PathVariable("id") long id, Model model) {
+        model.addAttribute("todo", todoRepository.findById(id).get());
+        return "todoform";
+    }
+
+    @RequestMapping("/delete/{id}")
+    public String delTODO(@PathVariable("id") long id) {
+        todoRepository.deleteById(id);
         return "redirect:/";
     }
 
